537ebf31dfb18c6691f5e9b80e50ed1d3e6868ba
Yang Enmanuel <enmnauelcalzat@gmail.com>
2022-12-12 18:29:58 -0300

    Added tailwind

--- src/AddressFinder.jsx
+++ src/AddressFinder.jsx
@@ -42,47 +42,69 @@ export default function AddressFinder() {
   }
 
   return (
-    <>
-      <h1>Address Finder</h1>
-      <form action='get' onSubmit={submitHandler}>
-        <input
-          required
-          type='text'
-          name='cep'
-          placeholder='Introduce a CEP'
-          onChange={validateCEP}
-          style={{ outline: 'none' }}
-        />
-        <button type='submit' disabled={cepIsValid ? false : true}>
-          Search
-        </button>
-      </form>
-
-      <div>
-        {Object.keys(address).length === 0 ? (
-          <p>Type a CEP!</p>
-        ) : (
-          <p>
-            {address.street ? address.street : '?'},
-            {address.neighborhood ? address.neighborhood : '?'},{address.city}
-          </p>
-        )}
-      </div>
-
-      <div className='mapWrapper'>
-        <Map
-          {...coordinates}
-          onMove={(e) => setCoordinates(e.viewState)}
-          mapboxAccessToken='pk.eyJ1IjoieWFuZ2VubWFudWVsIiwiYSI6ImNsYTg1YWdnMjAwZmszdm80NmV5cnU5YmoifQ.5rV6ex-488rO_XW8eWFujw'
-          mapStyle='mapbox://styles/mapbox/streets-v9'
+    <section className='text-gray-600 body-font'>
+      <div className='container mx-auto flex px-5 py-24 md:flex-row flex-col items-center'>
+        <div
+          className='lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0'
+          style={{ width: '600px', height: '400px' }}
         >
-          {/* If a valid CEP was submitted, draw a marker on the location */}
-          {Object.keys(address).length === 0 ? null : (
-            <Marker {...coordinates} anchor='bottom'></Marker>
+          <Map
+            {...coordinates}
+            onMove={(e) => setCoordinates(e.viewState)}
+            mapboxAccessToken='pk.eyJ1IjoieWFuZ2VubWFudWVsIiwiYSI6ImNsYTg1YWdnMjAwZmszdm80NmV5cnU5YmoifQ.5rV6ex-488rO_XW8eWFujw'
+            mapStyle='mapbox://styles/mapbox/streets-v9'
+          >
+            {Object.keys(address).length === 0 ? null : (
+              <Marker {...coordinates} anchor='bottom'></Marker>
+            )}
+            <NavigationControl />
+          </Map>
+        </div>
+
+        <div className='lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center'>
+          <h1 className='title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900'>
+            Address Finder
+          </h1>
+          <p className='mb-8 leading-relaxed'>
+            Type a valid CEP number to know exactly the position in the map
+          </p>
+          <form
+            action='get'
+            onSubmit={submitHandler}
+            className='flex w-full md:justify-start justify-center items-end'
+          >
+            <div className='relative mr-4 lg:w-full xl:w-1/2 w-2/4'>
+              {/* <label htmlFor='cep' className='leading-7 text-sm text-gray-600'> Type a CEP </label> */}
+
+              <input
+                className='w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:ring-2 focus:ring-indigo-200 focus:bg-transparent focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out'
+                required
+                type='text'
+                name='cep'
+                placeholder='Introduce a CEP'
+                onChange={validateCEP}
+              />
+            </div>
+            <button
+              type='submit'
+              disabled={cepIsValid ? false : true}
+              className='inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg'
+            >
+              Search
+            </button>
+          </form>
+
+          {Object.keys(address).length === 0 ? (
+            <p>Type a CEP!</p>
+          ) : (
+            <p className='mt-3'>
+              {address.street ? address.street : '?'},
+              {address.neighborhood ? address.neighborhood : '?'},{' '}
+              {address.city}
+            </p>
           )}
-          <NavigationControl />
-        </Map>
+        </div>
       </div>
-    </>
+    </section>
   )
 }
--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import CEPFinder from './CEPFinder'
 function App() {
   return (
     <>
-      {/* <AddressFinder /> */}
-      <CEPFinder />
+      <AddressFinder />
+      {/* <CEPFinder /> */}
     </>
   )
 }
