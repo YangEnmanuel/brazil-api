36c3cd7865ae13421a085bd735e310c0b0ad682a
Yang Enmanuel <enmanuelcalzat@gmail.com>
2022-12-20 21:39:07 -0300

    re-ordered the project structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import AddressFinder from './AddressFinder'
-import CEPFinder from './CEPFinder'
+import AddressFinder from './components/AddressFinder'
+import CEPFinder from './components/CEPFinder'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 
