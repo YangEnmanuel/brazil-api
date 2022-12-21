0a98166fcecd50b2b1cf38b4738f5873153a1689
Yang Enmanuel <enmanuelcalzat@gmail.com>
2022-12-21 13:40:17 -0300

    refactored Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,20 +3,25 @@ import imgUrl from '../assets/icons8-github-64.png'
 
 export default function NavBar() {
   return (
-    <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
-    <Container>
-      <Navbar.Brand href="#">Brasil Tools 🔨</Navbar.Brand>
-      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-      <Navbar.Collapse id="responsive-navbar-nav">
-        <Nav className="me-auto">
-          <Nav.Link href="#">Address Finder</Nav.Link>
-          <Nav.Link href="#">CEP Finder</Nav.Link>
-        </Nav>
-        <Nav className="">
-          <Nav.Link className='p-0 m-0'><img className='p-0' height={40} src={imgUrl} /></Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
-    </Container>
-  </Navbar>
+    <Navbar collapseOnSelect expand='lg' bg='light' variant='light'>
+      <Container>
+        <Navbar.Brand href='#'>Brasil Tools 🔨</Navbar.Brand>
+        <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+        <Navbar.Collapse id='responsive-navbar-nav'>
+          <Nav className='me-auto'>
+            <Nav.Link href='#'>Address Finder</Nav.Link>
+            <Nav.Link href='#'>CEP Finder</Nav.Link>
+          </Nav>
+          <Nav className=''>
+            <Nav.Link
+              href='https://github.com/yangenmanuel/brazil-tools'
+              className='p-0 m-0'
+            >
+              <img className='p-0' height={40} src={imgUrl} />
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   )
 }
