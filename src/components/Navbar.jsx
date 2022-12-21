4da690c3b3290e67fb071bd2042af9cc50df6b5c
Yang Enmanuel <enmanuelcalzat@gmail.com>
2022-12-21 13:39:58 -0300

    added github icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
-import { Nav, Navbar, Container } from 'react-bootstrap'
+import { Nav, Navbar, Container, Image } from 'react-bootstrap'
+import imgUrl from '../assets/icons8-github-64.png'
 
 export default function NavBar() {
   return (
@@ -8,10 +9,11 @@ export default function NavBar() {
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="me-auto">
-        </Nav>
-        <Nav className="">
           <Nav.Link href="#">Address Finder</Nav.Link>
           <Nav.Link href="#">CEP Finder</Nav.Link>
+        </Nav>
+        <Nav className="">
+          <Nav.Link className='p-0 m-0'><img className='p-0' height={40} src={imgUrl} /></Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Container>
